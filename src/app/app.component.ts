624bd64df9cad32f75c38f41a2269075ee7867b0
Laboratório FIAP <julia.maa77@gmail.com>
2025-04-10 20:26:44 -0300

    atualização com exercicio 09

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { MesComponent } from './components/mes/mes.component';
-import { ListComprasComponent } from './components/list-compras/list-compras.component';
 import { AcessoComponent } from './components/acesso/acesso.component';
-import { ListTarefaComponent } from './components/list-tarefa/list-tarefa.component';
 import { ExibirMensagensComponent } from './components/exibir-mensagens/exibir-mensagens.component';
+import { ListComprasComponent } from './components/list-compras/list-compras.component';
+import { ListTarefaComponent } from './components/list-tarefa/list-tarefa.component';
 import { ListaUsuariosComponent } from './components/lista-usuarios/lista-usuarios.component';
+import { MesComponent } from './components/mes/mes.component';
+import { SwitchNumerosComponent } from './components/switch-numeros/switch-numeros.component';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, MesComponent, ListComprasComponent, AcessoComponent, ListTarefaComponent, ExibirMensagensComponent, ListaUsuariosComponent],
+  imports: [RouterOutlet, CommonModule, MesComponent, ListComprasComponent, AcessoComponent, ListTarefaComponent, ExibirMensagensComponent, ListaUsuariosComponent, SwitchNumerosComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
