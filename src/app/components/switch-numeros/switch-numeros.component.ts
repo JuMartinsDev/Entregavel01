233dc21a73a66356fba94d41467989aea97fa57d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-10 19:38:14 -0300

    Exercicio 07 e 08

---
+++ src/app/components/switch-numeros/switch-numeros.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-numeros',
+  imports: [],
+  templateUrl: './switch-numeros.component.html',
+  styleUrl: './switch-numeros.component.css'
+})
+export class SwitchNumerosComponent {
+
+}
