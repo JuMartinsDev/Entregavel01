f97c42da4bbced1d35f1972dd2cfcd7f8cc9986c
JuMartinsDev <julia.maa77@gmail.com>
2025-04-10 15:52:45 -0300

    Exercicios iniciais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MesComponent } from './components/mes/mes.component';
+import { ListComprasComponent } from './components/list-compras/list-compras.component';
+import { AcessoComponent } from './components/acesso/acesso.component';
+import { ListTarefaComponent } from './components/list-tarefa/list-tarefa.component';
+import { ExibirMensagensComponent } from './components/exibir-mensagens/exibir-mensagens.component';
+import { ListaUsuariosComponent } from './components/lista-usuarios/lista-usuarios.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MesComponent, ListComprasComponent, AcessoComponent, ListTarefaComponent, ExibirMensagensComponent, ListaUsuariosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
